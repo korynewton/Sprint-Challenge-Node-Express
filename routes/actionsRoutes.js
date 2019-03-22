529120e443997ec8c7dcd4ee72a6b4a8f6097ca3
korynewton <korynewtondev@gmail.com>
2019-03-22 09:03:08 -0700

    GET routes in place for actions and projects, also for requests for individual id

--- routes/actionsRoutes.js
+++ routes/actionsRoutes.js
@@ -2,10 +2,36 @@ const express = require('express');
 
 const router = express.Router();
 
-// router.use(express.json());
+const Db = require('../data/helpers/actionModel')
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: "GET request to '/actions' working!"})
+router.use(express.json());
+
+router.get('/', async (req, res) => {
+    try {
+        const actions = await Db.get()
+        res.status(200).json(actions)
+    }
+    catch {
+        res.status(500).json({ "error": "error in retrieving actions"})
+    }
 })
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params; 
+    try {
+        const item = await Db.get(id)
+        if (!item) {
+            res.status(404).json({ "error" : "Item does not exist" })
+        }
+        else {
+            res.status(200).json(item)
+        }
+    }
+    catch {
+        res.status(500).json({ "error" : "error in retrieving that item" })
+    }
+})
+
+router
 
 module.exports = router;
--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -2,10 +2,35 @@ const express = require('express');
 
 const router = express.Router();
 
-// router.use(express.json());
+const Db = require('../data/helpers/projectModel')
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: "GET request to '/projects' working!"})
+router.use(express.json());
+
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Db.get()
+        res.status(200).json(projects)
+    }
+    catch {
+        res.status(500).json({ "error": "error in retrieving projects"})
+    }
 })
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params; 
+    try {
+        const item = await Db.get(id)
+        if (!item) {
+            res.status(404).json({ "error" : "Item does not exist" })
+        }
+        else {
+            res.status(200).json(item)
+        }
+    }
+    catch {
+        res.status(500).json({ "error" : "error in retrieving that item" })
+    }
+})
+
 
 module.exports = router;
